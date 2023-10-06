241b62aa71fd61ed18c08f6e603559a8f9a30937
Kevin.Bonnet <103254324+kkevin203@users.noreply.github.com>
2023-10-06 10:50:10 +0200

    ajout des detail animal

--- Database/Entities/Animal.cs
+++ Database/Entities/Animal.cs
@@ -15,11 +15,12 @@ namespace Database.Entities
         public string Name { get; set; }
         public float? Price { get; set; }
         public string? Race { get; set; }
+        public int? weight { get; set; }
         public string? Description { get; set; }
         public string? FarmerName { get; set; }
         public string? PhotoPath { get; set; }
-        public string? Categorisation { get; set; }
-        public bool? Stabu { get; set; }
+        public string? Categorization { get; set; }
+        public string? stabling { get; set; }
 
 
     }
