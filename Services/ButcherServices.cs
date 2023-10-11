6d48f43515b2c2cec757627d4fed3d0e1e5128f7
Kevin.Bonnet <103254324+kkevin203@users.noreply.github.com>
2023-10-11 11:56:39 +0200

    modification homepage - ajout registrationButcher et registrationFarmer modification Butcher pour ajout de butcher et filtre .

--- Services/ButcherServices.cs
+++ Services/ButcherServices.cs
@@ -1,5 +1,6 @@
 ﻿using Database;
 using Database.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,11 +18,50 @@ namespace Services
             _butcherDatabase = butcherDatabase;
         }
 
-        public List<Butcher> GetAll()
+        public async Task<List<Butcher>> GetAllAsync()
         {
-            return new List<Butcher>() { new Butcher() { Name = "Kevin", Email = "exemple@gmail.com" } };
-            var butchers = _butcherDatabase.Butchers.ToList();
-            return butchers;
+            try
+            {
+                return await _butcherDatabase.Butchers.ToListAsync();
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
+        public Butcher GetButcheryId(int butcherId)
+        {
+            var butcher = _butcherDatabase.Butchers.FirstOrDefault(f => f.Id == butcherId);
+            return butcher;
+        }
+        public async Task<List<Butcher>> GetButcherAsync()
+        {
+            try
+            {
+                var butchers = await _butcherDatabase.Butchers.ToListAsync();
+                return butchers;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        public async Task CreateButcherAsync(Butcher newButcher)
+        {
+            try
+            {
+
+
+                _butcherDatabase.Butchers.Add(newButcher);
+                await _butcherDatabase.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+               
+                throw;
+            }
+        }
+
     }
 }
